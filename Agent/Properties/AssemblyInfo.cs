845238f778d0086f4f73b9553096e051ae0d1e5b
fordster78 <jack-ford@outlook.com>
2017-12-27 15:25:47 +0000

    Update Version details

--- Agent/Properties/AssemblyInfo.cs
+++ Agent/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Agent")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Snipeit Agent")]
+[assembly: AssemblyDescription("Works with Snipe-it V4")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard")]
-[assembly: AssemblyProduct("Agent")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2017")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Snipeit Agent")]
+[assembly: AssemblyCopyright("Copyright © Fordster78 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
